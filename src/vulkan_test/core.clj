ae31e029a95d430ff77a117329147086d2ad53dc
Jukka Villstedt <jukka.villstedt@iki.fi>
2021-12-16 08:03:03 +0200

    extract main-thread

--- src/vulkan_test/core.clj
+++ src/vulkan_test/core.clj
@@ -4,11 +4,8 @@
            [org.lwjgl.system MemoryUtil])
   (:require [vulkan-test.queue :as queue]
             [vulkan-test.cider-repl :as cider-repl]
-            [nrepl.server :as server]
-            [cider.nrepl.middleware :as middleware]
-            [refactor-nrepl.middleware :as refactor-nrepl-middleware])
+            [vulkan-test.main-thread :as main-thread])
   (:gen-class))
-
 
 
 (defn initialize-glfw []
@@ -33,70 +30,23 @@
   (Callbacks/glfwFreeCallbacks window-handle)
   (GLFW/glfwDestroyWindow window-handle))
 
-
-;; command loop
-
-(defn start-command-loop [should-terminate?-atom command-queue result-queue]
-  (loop [function nil]
-    (poll-events)
-
-    (when function
-      (println "running in thread " (.getId (Thread/currentThread))) ;; TODO: remove-me
-      (queue/put result-queue (or (function)
-                                  :nil)))
-
-    (when (not @should-terminate?-atom)
-      (recur (queue/take command-queue
-                         500)))))
-
-(defn start-main-command-loop []
-  (def should-terminate?-atom (atom false))
-  (def command-queue (queue/queue))
-  (def result-queue (queue/queue))
-  (start-command-loop should-terminate?-atom
-                      command-queue
-                      result-queue))
-
-(defn terminate-main-command-loop []
-  (reset! should-terminate?-atom true))
-
-(defn run-in-main-thread [function]
-  (queue/put command-queue function)
-  (queue/take result-queue))
-
-(defmacro in-main-thread [& body]
-  `(run-in-main-thread (fn [] ~@body)))
-
-
-
 (defn -main
   [& _args]
   (cider-repl/start-cider-nrepl)
   (initialize-glfw)
 
-  ;; (def window-handle (create-window "Vulkan Test"
-  ;;                                   100 100))
-
-  ;; (while (not (GLFW/glfwWindowShouldClose window-handle))
-  ;;   (poll-events))
-
-  ;; (destroy-window window-handle)
-
-  (start-main-command-loop)
+  (main-thread/enter-main-thread-command-loop 500
+                                              poll-events)
 
   (terminate-glfw)
-  (prn "glfw terminated")
-  (System/exit 0)
-
-  )
-
+  (System/exit 0))
 
 
 (comment
-  (in-main-thread (def window-handle (create-window "Vulkan Test"
-                                                    100 100)))
-  (GLFW/glfwWindowShouldClose window-handle)
-  (in-main-thread (destroy-window window-handle))
-  (terminate-main-command-loop)
+  (main-thread/in-main-thread (def window-handle (create-window "Vulkan Test"
+                                                                100 100)))
+  (main-thread/in-main-thread (GLFW/glfwWindowShouldClose window-handle))
+  (main-thread/in-main-thread (destroy-window window-handle))
+  (main-thread/terminate-main-thread-command-loop)
 
   )
---
+++ src/vulkan_test/main_thread.clj
@@ -0,0 +1,38 @@
+(ns vulkan-test.main-thread
+  (:require [vulkan-test.queue :as queue]))
+
+
+(defn- start-command-loop [poll-timeout run-on-every-poll should-terminate?-atom command-queue result-queue]
+  (loop [function nil]
+    (run-on-every-poll)
+
+    (when function
+      (println "running in thread " (.getId (Thread/currentThread))) ;; TODO: remove-me
+      (queue/put result-queue (let [result (function)]
+                                (if (nil? result)
+                                  :nil
+                                  result))))
+
+    (when (not @should-terminate?-atom)
+      (recur (queue/take command-queue
+                         poll-timeout)))))
+
+(defn enter-main-thread-command-loop [poll-timeout run-on-every-poll]
+  (def should-terminate?-atom (atom false))
+  (def command-queue (queue/queue))
+  (def result-queue (queue/queue))
+  (start-command-loop poll-timeout
+                      run-on-every-poll
+                      should-terminate?-atom
+                      command-queue
+                      result-queue))
+
+(defn terminate-main-thread-command-loop []
+  (reset! should-terminate?-atom true))
+
+(defn run-in-main-thread [function]
+  (queue/put command-queue function)
+  (queue/take result-queue))
+
+(defmacro in-main-thread [& body]
+  `(run-in-main-thread (fn [] ~@body)))
