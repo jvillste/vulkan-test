a2e67566734905a81002118381082a3e74384ea0
Jukka Villstedt <jukka.villstedt@iki.fi>
2021-12-18 07:08:10 +0200

    add event callbacks for created windows

--- src/vulkan_test/core.clj
+++ src/vulkan_test/core.clj
@@ -1,12 +1,10 @@
 (ns vulkan-test.core
-  (:import [org.lwjgl.vulkan VK11]
-           [org.lwjgl.glfw GLFW GLFWVulkan Callbacks]
+  (:import ;; [org.lwjgl.vulkan]
+           [org.lwjgl.glfw GLFW Callbacks GLFWFramebufferSizeCallback GLFWKeyCallback GLFWCursorPosCallback GLFWCursorEnterCallback GLFWMouseButtonCallback]
            [org.lwjgl.system MemoryUtil])
-  (:require [vulkan-test.queue :as queue]
-            [vulkan-test.cider-repl :as cider-repl]
+  (:require [vulkan-test.cider-repl :as cider-repl]
             [vulkan-test.main-thread :as main-thread])
   (:gen-class))
-
 
 (defn initialize-glfw []
   (GLFW/glfwInit))
@@ -14,14 +12,54 @@
 (defn terminate-glfw []
   (GLFW/glfwTerminate))
 
-(defn create-window [title width height]
+(defn create-window [title width height event-callback]
   (GLFW/glfwWindowHint GLFW/GLFW_CLIENT_API GLFW/GLFW_NO_API)
 
-  (GLFW/glfwCreateWindow width
-                         height
-                         title
-                         MemoryUtil/NULL
-                         MemoryUtil/NULL))
+  (let [window-handle (GLFW/glfwCreateWindow width
+                                             height
+                                             title
+                                             MemoryUtil/NULL
+                                             MemoryUtil/NULL)]
+
+    (GLFW/glfwSetFramebufferSizeCallback window-handle
+                                         (proxy [GLFWFramebufferSizeCallback] []
+                                           (invoke [_window-handle width height]
+                                             (event-callback {:type :framebuffer-resized
+                                                              :width width
+                                                              :height height}))))
+
+    (GLFW/glfwSetKeyCallback window-handle
+                             (proxy [GLFWKeyCallback] []
+                               (invoke [_window-handle key scancode action modifiers]
+                                 (event-callback {:type (if (= GLFW/GLFW_PRESS action)
+                                                          :key-pressed
+                                                          :key-released)
+                                                  :scancode scancode
+                                                  :modifiers modifiers}))))
+
+    (GLFW/glfwSetCursorPosCallback window-handle
+                                   (proxy [GLFWCursorPosCallback] []
+                                     (invoke [_window-handle x y]
+                                       (event-callback {:type :mouse-moved
+                                                        :x x
+                                                        :y y}))))
+
+    (GLFW/glfwSetCursorEnterCallback window-handle
+                                     (proxy [GLFWCursorEnterCallback] []
+                                       (invoke [_window-handle entered?]
+                                         (event-callback {:type (if entered?
+                                                                  :mouse-entered
+                                                                  :mouse-left)}))))
+
+    (GLFW/glfwSetMouseButtonCallback window-handle
+                                     (proxy [GLFWMouseButtonCallback] []
+                                       (invoke [_window-handle button action modifiers]
+                                         (event-callback {:type (if (= GLFW/GLFW_PRESS action)
+                                                                  :mouse-button-pressed
+                                                                  :mouse-button-released)
+                                                          :button button
+                                                          :modifiers modifiers}))))
+    window-handle))
 
 (defn poll-events []
   (GLFW/glfwPollEvents))
@@ -42,8 +80,13 @@
 
 
 (comment
+
   (main-thread/in-main-thread (def window-handle (create-window "Vulkan Test"
-                                                                100 100)))
+                                                                100
+                                                                100
+                                                                (fn [event]
+                                                                  (prn event)))))
+
   (main-thread/in-main-thread (GLFW/glfwWindowShouldClose window-handle))
   (main-thread/in-main-thread (destroy-window window-handle))
   (main-thread/terminate-command-loop)
